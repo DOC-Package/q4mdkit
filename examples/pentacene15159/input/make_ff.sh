277a9a39010053fafd63682f3c55270770a124e0
Hideaki Takahashi <takahashi.hideaki.w33@kyoto-u.jp>
2025-12-09 22:14:17 +0100

    Update examples

--- examples/pentacene15159/input/make_ff.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-antechamber -i pentacene_template.mol2 -fi mol2 -o pentacene_gaff2.mol2 -fo mol2 -at gaff2 -c bcc -nc 0 -rn PEN
-parmchk2 -i pentacene_gaff2.mol2 -f mol2 -o pentacene.frcmod -s gaff2
-tleap -f tleap.in
-#cpptraj -i setbox.cpptraj
