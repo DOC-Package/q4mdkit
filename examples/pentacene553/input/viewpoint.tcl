cc25ebe97053d3666cc6db38f30926bc31d2303a
Hideaki Takahashi <takahashi.hideaki.w33@kyoto-u.jp>
2026-01-14 16:47:05 +0100

    Update examples

--- examples/pentacene553/input/viewpoint.tcl
+++
@@ -1,5 +0,0 @@
-# VMD viewpoint settings - auto-generated
-molinfo top set {rotate_matrix} {{{0.751197 -0.659324 0.0314761 0} {0.655324 0.750648 0.084071 0} {-0.0790562 -0.042528 0.995958 0} {0 0 0 1}}}
-molinfo top set {center_matrix} {{{1 0 0 -18.3775} {0 1 0 -25.3198} {0 0 1 -19.6796} {0 0 0 1}}}
-molinfo top set {scale_matrix} {{{0.0470373 0 0 0} {0 0.0470373 0 0} {0 0 0.0470373 0} {0 0 0 1}}}
-molinfo top set {global_matrix} {{{1 0 0 -6.55651e-07} {0 1 0 -7.7486e-07} {0 0 1 1.3113e-06} {0 0 0 1}}}
